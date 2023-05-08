606b8cb9bc88397763533121575e3d4a65512faf
Maksim Saykovsky <saykovskiy.maks03@gmail.com>
2023-05-08 22:55:53 +0300

    migrations to MySQL

--- src/settings.py
+++ src/settings.py
@@ -88,12 +88,32 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASE_NAME = os.getenv('DATABASE_NAME', 'postgres')
+DATABASE_USER = os.getenv('DATABASE_USER', 'postgres')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', 'postgres')
+DATABASE_HOST = os.getenv('DATABASE_HOST', 'db')
+DATABASE_PORT = os.getenv('DATABASE_PORT', '5432')
+
+USE_SQLITE = os.environ.get('USE_SQLITE', 'False')
+
+if USE_SQLITE == 'True':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': DATABASE_NAME,
+            'USER': DATABASE_USER,
+            'PASSWORD': DATABASE_PASSWORD,
+            'HOST': DATABASE_HOST,
+            'PORT': DATABASE_PORT,
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
